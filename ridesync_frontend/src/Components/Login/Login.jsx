70f730d4576c1c7d2ad4c8a586e9d778c338128d
Sivasubramanian Venkatasubramanian <sv386677@dal.ca>
2024-02-06 14:36:07 -0400

    Feat#2: API integrations in UI

--- ridesync_frontend/src/Components/Login/Login.jsx
+++ ridesync_frontend/src/Components/Login/Login.jsx
@@ -1,8 +1,27 @@
 import { Box, Button, Flex, Image, Input, Text } from '@chakra-ui/react'
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleSubmit = () => {
+        let requestObj = {
+            email: email,
+            password: password
+        }
+        axios.post('http://localhost:8073/api/v1/auth/authenticate', requestObj)
+            .then(response => {
+                console.log('Response:', response);
+                let { token, user } = response
+                localStorage.setItem('loggedInUserDetails', { token, user });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
     return (
         <Box  >
             <Flex w={["100%", "95%", "90%", "80%"]} h={["", "", "100vh", "100vh"]} m="auto" justifyContent="center" align="center" flexDir={["column", "column", "row", "row"]}>
@@ -12,11 +31,15 @@ const Login = () => {
                 <Flex w={["100%", "95%", "60%", "50%"]} justifyContent="center" align="center" flexDir="column" >
                     <Text fontWeight="medium" fontSize="3xl">Login</Text>
                     <br />
-                    <Input w="75%" placeholder='email'></Input>
+                    <Input w="75%" placeholder='email' onChange={(e) => setEmail(e.target.value)}></Input>
                     <br />
-                    <Input w="75%" placeholder='password'></Input>
+                    <Input w="75%"
+                        placeholder='password'
+                        type='password'
+                        onChange={(e) => setPassword(e.target.value)}>
+                    </Input>
                     <br />
-                    <Button w="75%" colorScheme='blue'>Submit</Button>
+                    <Button w="75%" colorScheme='blue' onClick={handleSubmit}>Submit</Button>
                     <br />
                     <span >Haven't registered yet ? <span className='text-blue-700'><Link to="/signup">Click here</Link></span></span>
                 </Flex>
--- ridesync_frontend/src/Components/Signup/Signup.jsx
+++ ridesync_frontend/src/Components/Signup/Signup.jsx
@@ -1,8 +1,46 @@
-import { Box, Button, Flex, Image, Input, Text } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Box, Button, Flex, Image, Input, Text, useToast } from '@chakra-ui/react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+    const [userDetails, setUserDetails] = useState({
+        "fullName": "",
+        "email": "",
+        "address": "",
+        "dateOfBirth": "",
+        "phoneNumber": "",
+        "password": ""
+    })
+    let navigate = useNavigate();
+    const toast = useToast();
+
+    const registerUser = () => {
+        // userDetails is the request body
+        console.log(userDetails)
+        axios.post('http://localhost:8073/api/v1/auth/register', userDetails)
+            .then(response => {
+                console.log('Response:', response);
+                toast({
+                    title: 'Account created.',
+                    status: 'success',
+                    duration: 9000,
+                    isClosable: true,
+                })
+                navigate('/login');
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+    const setUserDetail = (e) => {
+        // e is an event object, it would contain name of the input field and value of the input field
+        const { name, value } = e.target;
+        setUserDetails(prevDetails => ({
+            ...prevDetails,
+            [name]: value
+        }));
+    };
     return (
         <Box>
             <br />
@@ -12,21 +50,51 @@ const Signup = () => {
 
                     <Text fontWeight="medium" fontSize="3xl" textAlign="center">Create account</Text>
                     <Text mb="1">Name</Text>
-                    <Input w={["100%", "100%", "90%", "90%"]} placeholder='Enter your name' type='text'></Input>
+                    <Input w={["100%", "100%", "90%", "90%"]}
+                        placeholder='Enter your name'
+                        type='text'
+                        name="fullName"
+                        value={userDetails.fullName}
+                        onChange={setUserDetail}></Input>
                     <Text mt="3" mb="1">Email</Text>
 
-                    <Input w={["100%", "100%", "90%", "90%"]} placeholder='Enter your email' type='email'></Input>
+                    <Input w={["100%", "100%", "90%", "90%"]}
+                        placeholder='Enter your email'
+                        type='email'
+                        name="email"
+                        value={userDetails.email}
+                        onChange={setUserDetail}></Input>
                     <Text mt="3" mb="1">Address</Text>
-                    <Input w={["100%", "100%", "90%", "90%"]} placeholder='Enter your address' type='text'></Input>
+                    <Input w={["100%", "100%", "90%", "90%"]}
+                        placeholder='Enter your address'
+                        type='text'
+                        name="address"
+                        value={userDetails.address}
+                        onChange={setUserDetail}></Input>
                     <Text mt="3" mb="1">Date of birth</Text>
 
-                    <Input w={["100%", "100%", "90%", "90%"]} placeholder='Date of Birth' type='date'></Input>
+                    <Input w={["100%", "100%", "90%", "90%"]}
+                        placeholder='Date of Birth'
+                        type='date'
+                        name="dateOfBirth"
+                        value={userDetails.dateOfBirth}
+                        onChange={setUserDetail}></Input>
                     <Text mt="3" mb="1">Phone number</Text>
-                    <Input w={["100%", "100%", "90%", "90%"]} placeholder='Enter your phone number' type='number'></Input>
+                    <Input w={["100%", "100%", "90%", "90%"]}
+                        placeholder='Enter your phone number'
+                        type='number'
+                        name="phoneNumber"
+                        value={userDetails.phoneNumber}
+                        onChange={setUserDetail}></Input>
                     <Text mt="3" mb="1">Password</Text>
-                    <Input w={["100%", "100%", "90%", "90%"]} placeholder='Enter your password' type='password'></Input>
+                    <Input w={["100%", "100%", "90%", "90%"]}
+                        placeholder='Enter your password'
+                        type='password'
+                        name="password"
+                        value={userDetails.password}
+                        onChange={setUserDetail}></Input>
                     <br />
-                    <Button w={["100%", "100%", "90%", "90%"]} colorScheme='blue'>Submit</Button>
+                    <Button w={["100%", "100%", "90%", "90%"]} colorScheme='blue' onClick={registerUser}>Submit</Button>
                     <br />
                     <span >Already have an account? <span className='text-blue-700'><Link to="/login">Click here</Link></span></span>
                 </Flex>
