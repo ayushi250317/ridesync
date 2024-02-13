67ad85640a797d9f1dcd58750846469ce8d075da
nithinbharathi <nithinbharathik@outlook.com>
2024-02-12 21:49:14 -0400

    feature4: introduced api for route coordinate serialization

---
+++ ridesync_backend/src/main/java/com/app/ridesync/entities/GeoPointRecord.java
@@ -0,0 +1,11 @@
+package com.app.ridesync.entities;
+
+import java.util.List;
+
+import com.google.maps.model.LatLng;
+
+public record GeoPointRecord (List<LatLng> geoPoints){
+	public List<LatLng> getGeoPoints() {
+		return geoPoints;
+	}
+}
