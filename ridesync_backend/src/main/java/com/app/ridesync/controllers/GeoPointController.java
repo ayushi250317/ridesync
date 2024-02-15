294fbb77d5be0834a6410d5472c6d2dc975c9bb8
nithinbharathi <nithinbharathik@outlook.com>
2024-02-14 22:37:40 -0400

    fix: changed the datatype of GeoPoint to BLOB

--- ridesync_backend/src/main/java/com/app/ridesync/controllers/GeoPointController.java
+++ ridesync_backend/src/main/java/com/app/ridesync/controllers/GeoPointController.java
@@ -1,26 +1,27 @@
 package com.app.ridesync.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.app.ridesync.entities.GeoPoint;
 import com.app.ridesync.services.GeoPointService;
 
 @RequestMapping(path = "api/v1/geo")
-@Controller
+@RestController
 public class GeoPointController {		
 	private final GeoPointService geoPointService;
-	
+
 	@Autowired
 	public GeoPointController(GeoPointService geoPointService) {
 		this.geoPointService = geoPointService;
 	}
 	
-	@PostMapping
-	public void saveGeoPoints(@RequestBody GeoPoint geoPoints) {
-		geoPointService.saveGeoPoints(geoPoints);
+	@PostMapping("/create")
+	public ResponseEntity<String> create(@RequestBody GeoPoint geoPoints) {
+		return geoPointService.saveGeoPoints(geoPoints);			
 	}
 }
--- ridesync_backend/src/main/java/com/app/ridesync/entities/GeoPoint.java
+++ ridesync_backend/src/main/java/com/app/ridesync/entities/GeoPoint.java
@@ -2,11 +2,15 @@ package com.app.ridesync.entities;
 
 import com.app.ridesync.converter.GeoPointConverter;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,7 +22,13 @@ public class GeoPoint {
 	private long id;
 	
 	@Convert(converter = GeoPointConverter.class)	
-	private GeoPointRecord geoPointRecord;
+	@Lob
+	@Column(length = (int)2e24-1)
+	private GeoPointRecord geoPointRecord;			// a length of 2^24 is treated as mediumBlob in mysql and can store ~16MB of data.
+	
+	@OneToOne
+	@JoinColumn(name = "ride_id")
+	//private Ride rideId;				TODO: changes will be made once the post a ride functionality is pushed.
 
 	public long getId() {
 		return id;
