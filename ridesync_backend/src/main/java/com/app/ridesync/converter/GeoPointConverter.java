c18c12d3e9a2e9ad79dbc5eed7893bae402aad7c
nithinbharathi <nithinbharathik@outlook.com>
2024-02-19 10:10:02 -0400

    fix: changed the datatype of GeoPoint to BLOB

--- ridesync_backend/src/main/java/com/app/ridesync/converter/GeoPointConverter.java
+++ ridesync_backend/src/main/java/com/app/ridesync/converter/GeoPointConverter.java
@@ -2,15 +2,19 @@ package com.app.ridesync.converter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 import com.app.ridesync.config.ObjectMapperConfig;
+import com.app.ridesync.entities.GeoPoint;
 import com.app.ridesync.entities.GeoPointRecord;
+import com.google.maps.model.LatLng;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import java.lang.RuntimeException;
+import java.util.List;
 
 @Converter
-public class GeoPointConverter implements AttributeConverter<GeoPointRecord,String>{
+public class GeoPointConverter implements AttributeConverter<GeoPointRecord,byte[]>{
 	private final ObjectMapperConfig objectMapperConfig;
 
 	@Autowired
@@ -19,16 +23,16 @@ public class GeoPointConverter implements AttributeConverter<GeoPointRecord,Stri
 	}
 
 	@Override
-	public String convertToDatabaseColumn(GeoPointRecord geoPoints) {		
+	public byte[] convertToDatabaseColumn(GeoPointRecord geoPoints) {		
 		try {
-			return objectMapperConfig.objectMapper().writeValueAsString(geoPoints);
+			return objectMapperConfig.objectMapper().writeValueAsBytes(geoPoints);
 		} catch (Exception e) {
 			throw new RuntimeException("Error serializing the route coordinates to JSON.", e);
 		}
 	}
 
 	@Override
-	public GeoPointRecord convertToEntityAttribute(String geoPoints) {
+	public GeoPointRecord convertToEntityAttribute(byte[] geoPoints) {
 		try {
 			return objectMapperConfig.objectMapper().readValue(geoPoints,GeoPointRecord.class);
 		} catch (Exception e) {
