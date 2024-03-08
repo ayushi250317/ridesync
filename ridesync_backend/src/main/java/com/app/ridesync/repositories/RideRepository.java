8bc80db8f317486a1ae7b6d5a803b4cc18898eda
nithinbharathi <nithinbharathi@dal.ca>
2024-03-07 23:52:21 -0400

    Bug #62 : included condition to fetch only active/posted rides

--- ridesync_backend/src/main/java/com/app/ridesync/repositories/RideRepository.java
+++ ridesync_backend/src/main/java/com/app/ridesync/repositories/RideRepository.java
@@ -27,7 +27,7 @@ public interface RideRepository extends JpaRepository<Ride, Integer> {
 			+ " JOIN Location startLocation ON startLocation.locationId = rideInfo.startLocationId"
 			+ " JOIN Location endLocation ON endLocation.locationId = rideInfo.endLocationId"
 			+ " JOIN Vehicle vehicle on vehicle.vehicleId = ride.vehicleId"
-			+ " WHERE ride.startTime >= :rideTimeStartLimit AND ride.startTime <= :rideTimeEndLimit AND ride.rideId IN :rideIds"
+			+ " WHERE ride.status != 'completed' AND ride.startTime >= :rideTimeStartLimit AND ride.startTime <= :rideTimeEndLimit AND ride.rideId IN :rideIds"
 			+ " ORDER BY ride.startTime")
 	List<SearchResultProjection>findByRideIds(@Param("rideIds") List<Integer> rideIds, @Param("rideTimeStartLimit") LocalDateTime rideTimeStartLimit, @Param("rideTimeEndLimit") LocalDateTime rideTimeEndLimit);
 
