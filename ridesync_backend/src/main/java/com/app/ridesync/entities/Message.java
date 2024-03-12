456e6725db46c74500c325917f35806b5fc71f8b
nithinbharathi <nithinbharathi@dal.ca>
2024-03-12 07:29:39 -0300

    added payload test for get messages API

--- ridesync_backend/src/main/java/com/app/ridesync/entities/Message.java
+++ ridesync_backend/src/main/java/com/app/ridesync/entities/Message.java
@@ -2,18 +2,24 @@ package com.app.ridesync.entities;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Message {
 	@Id
+	@Column(name = "message_id")
     @GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer messageId;
 	private Integer senderId;
