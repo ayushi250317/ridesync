39a8e73c6b114c38d49de28957b72b931714c56c
Meer Patel <meerpatel.1616@gmail.com>
2024-03-17 16:14:29 -0300

    added env variables

--- ridesync_backend/src/main/java/com/app/ridesync/services/AuthenticationService.java
+++ ridesync_backend/src/main/java/com/app/ridesync/services/AuthenticationService.java
@@ -14,7 +14,7 @@ import jakarta.mail.internet.MimeMessage;
 import com.app.ridesync.entities.Document;
 import com.app.ridesync.entities.User;
 import com.app.ridesync.entities.Vehicle;
-
+import org.springframework.core.env.Environment;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +24,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import org.springframework.beans.factory.annotation.Value;
 import lombok.RequiredArgsConstructor;
 
 
@@ -32,6 +32,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthenticationService {
 	
+    @Autowired
+    private Environment env;
 	@Autowired
     private final UserRepository repository;
     @Autowired
@@ -47,6 +49,12 @@ public class AuthenticationService {
     @Autowired
     private final JavaMailSender javaMailSender;
 
+    @Value("${IP_ADDRESS}")
+    private String ip_address;
+
+    @Value("${FRONTEND_PORT}")
+    private String port;
+
     public AuthenticationResponse validateRequest(RegisterRequest request) throws MessagingException {
         User user=repository.findByEmail(request.getEmail());
         if(user!=null){
@@ -56,6 +64,8 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse register(RegisterRequest request) throws MessagingException {
+
+
         User user = User.builder()
                 .fullName(request.getFullName())
                 .email(request.getEmail())
@@ -70,7 +80,7 @@ public class AuthenticationService {
         message.setRecipients(MimeMessage.RecipientType.TO,request.getEmail());
         message.setSubject("Verify Ridesync Account");
 //        String htmlContent="<p>Click the <a href=\"http://172.17.1.101:3000/confirm_registration/"+user.getUserId()+"/"+user.getEmail()+"\">link</a> to verify your email </p>";
-        String htmlContent="<p>Click the <a href=\"http://localhost:3000/confirm_registration/"+user.getUserId()+"/"+user.getEmail()+"\">link</a> to verify your email </p>";
+        String htmlContent="<p>Click the <a href=\"http://"+ip_address+":"+port+"/confirm_registration/"+user.getUserId()+"/"+user.getEmail()+"\">link</a> to verify your email </p>";
         message.setContent(htmlContent,"text/html;charset=utf-8");
         javaMailSender.send(message);
         return AuthenticationResponse.builder()
@@ -118,6 +128,8 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse forgotPassword(AuthenticationRequest request) throws MessagingException {
+        String ip_address = env.getProperty("IP_ADDRESS");
+        String port = env.getProperty("FRONTEND_PORT");
       User user=repository.findByEmail(request.getEmail());
       if(user==null){
         return AuthenticationResponse.builder().message("Email do not exist").build();
@@ -128,7 +140,7 @@ public class AuthenticationService {
       message.setRecipients(MimeMessage.RecipientType.TO,request.getEmail());
       message.setSubject("Reset Password");
 //      String htmlContent="<p>Click the <a href=\"http://172.17.1.101:3000/confirm_password/"+resetToken+"/"+user.getUserId()+"\">link</a> to reset your password </p>";
-      String htmlContent="<p>Click the <a href=\"http://localhost:3000/confirm_password/"+resetToken+"/"+user.getUserId()+"\">link</a> to reset your password </p>";
+      String htmlContent="<p>Click the <a href=\"http://"+ip_address+":"+port+"/confirm_password/"+resetToken+"/"+user.getUserId()+"\">link</a> to reset your password </p>";
       message.setContent(htmlContent,"text/html;charset=utf-8");
       javaMailSender.send(message);
       return AuthenticationResponse.builder().message("email sent successfully").success(true).build();
