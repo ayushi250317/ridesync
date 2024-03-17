e865e5430e37aec8e7b6ac348ebedda662165561
ayushim <ay543098@dal.ca>
2024-03-16 22:10:03 -0300

    Modified Response for Get Ride Requests Functionality

--- ridesync_backend/src/main/java/com/app/ridesync/repositories/RideRequestRepository.java
+++ ridesync_backend/src/main/java/com/app/ridesync/repositories/RideRequestRepository.java
@@ -3,15 +3,25 @@ package com.app.ridesync.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.app.ridesync.entities.RideInfo;
 import com.app.ridesync.entities.RideRequestInfo;
+import com.app.ridesync.projections.RideRequestProjection;
 
 
 @Repository
 public interface RideRequestRepository extends JpaRepository<RideRequestInfo,Integer>{
-    List<RideRequestInfo> findByRideId(Integer rideId);
+    @Query("SELECT new com.app.ridesync.projections.RideRequestProjection(rideRequest.rideRequestId as rideRequestId, rideRequest.rideId as rideId,rideRequest.riderId as riderId,rideRequest.driverId as driverId,rideRequest.tripStartTime, rideRequest.requestStatus as requestStatus, pickupLocation.address as pickupAddress, pickupLocation.lattitude as pickupLat, pickupLocation.longitude as pickupLong, dropLocation.address as dropAddress, dropLocation.lattitude as dropLat, dropLocation.longitude as dropLong, user.fullName as riderName)"
+			+ " FROM RideRequestInfo rideRequest "
+			+ " JOIN Location pickupLocation ON pickupLocation.locationId = rideRequest.startLocationId"
+			+ " JOIN Location dropLocation ON dropLocation.locationId = rideRequest.endLocationId"
+			+ " JOIN User user ON user.userId=rideRequest.riderId"
+			+ " WHERE  rideRequest.rideId=:rideId"
+			)
+    List<RideRequestProjection> findByRideId(@Param("rideId") Integer rideId);
     RideRequestInfo findByRideRequestId(Integer rideRequestId);
 
 }
