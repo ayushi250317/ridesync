4c3d2f19bad7ad794a84f2125ea3206fe585aedd
Meer Patel <meerpatel.1616@gmail.com>
2024-03-20 12:27:27 -0300

    fix: resolving gmap api key and env issue

--- ridesync_frontend/src/sharedComponent/RideInfo/RideInfo.jsx
+++ ridesync_frontend/src/sharedComponent/RideInfo/RideInfo.jsx
@@ -16,7 +16,7 @@ const libraries = ['places'];
 
 const Activity = ({ route }) => {
     const { isLoaded } = useJsApiLoader({
-        googleMapsApiKey: "AIzaSyAgF12Bol5WhhLKL8WFlTc-qjMlPjugM3A",
+        googleMapsApiKey: GMAP_API_KEY,
         libraries,
     });
     const backbtn = "<"
