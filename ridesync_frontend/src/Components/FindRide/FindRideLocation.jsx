eb84216cb21c270daf83aeea182e2a440573299e
Meer Patel <meerpatel.1616@gmail.com>
2024-03-21 14:54:16 -0300

    added post call in find ride

--- ridesync_frontend/src/Components/FindRide/FindRideLocation.jsx
+++ ridesync_frontend/src/Components/FindRide/FindRideLocation.jsx
@@ -49,7 +49,7 @@ const FindRideLocation = () => {
         const config = {
             headers: { Authorization: `Bearer ${loggedInUserDetails.token}` },
         };
-        axios.get(`${API}/geo/search`, newObj, config).then(resp => {
+        axios.post(`${API}/geo/search`, newObj, config).then(resp => {
             console.log("search ride", resp.data);
             if (resp.data.success) {
                 setFindRidesResult(resp.data.responseObject)
