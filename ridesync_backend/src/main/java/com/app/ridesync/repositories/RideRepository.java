2104c8861b94717ae70c38b6a24e1bcb26e32d41
nithinbharathi <nithinbharathi@dal.ca>
2024-03-22 19:46:33 -0300

    Bug fix: #87 changed the logic to set enable ride flag by replacing subquery logic with a join

--- ridesync_backend/src/main/java/com/app/ridesync/repositories/RideRepository.java
+++ ridesync_backend/src/main/java/com/app/ridesync/repositories/RideRepository.java
@@ -43,11 +43,9 @@ public interface RideRepository extends JpaRepository<Ride, Integer> {
 	List<RideHistoryProjection> findRidesByUserId(@Param("userId") Integer userId);
 
 	@Query ("SELECT "
-			+ "NEW com.app.ridesync.projections.RideInfoProjection(user.fullName AS riderName,user.userId riderId ,rideInfo.isDriver, "
+			+ "NEW com.app.ridesync.projections.RideInfoProjection(user.fullName AS riderName, rideInfo.isDriver, "
 			+ "startLocation.address AS startLocationAddress, startLocation.landmark AS startLocationLandmark, "
-			+ "startLocation.lattitude startLat, startLocation.longitude startLong, "
 			+ "endLocation.address AS endLocationAddress, endLocation.landmark AS endLocationLandmark, "
-			+ "endLocation.lattitude endLat, endLocation.longitude endLong, "
 			+ "rideInfo.fare,rideInfo.comments,rideInfo.rating,rideInfo.waitTime,rideInfo.estimatedTripStartTime AS riderTripStartTime, rideInfo.estimatedTripEndTime AS riderTripEndTime) "
 
 			+ "FROM RideInfo rideInfo "
@@ -60,15 +58,11 @@ public interface RideRepository extends JpaRepository<Ride, Integer> {
 	@Query ("SELECT "
 			+ "NEW com.app.ridesync.projections.RideHeaderProjection(ride.description,ride.startTime AS originalTripStartTime, "
 			+ "ride.createdTime as tripPostedTime, ride.status, "
-			+ "ride.seatsAvailable,"
-			+ "startLocation.address startLocationAddress, "
-			+ "endLocation.address endLocationAddress,"
-			+ "vehicle.model AS rideVehicle) "
+			+ "vehicle.model AS rideVehicle, "
+			+ "geoPoint.geoPointRecord as routeCoordinates) "		
 			+ "FROM Ride ride "
-			+ "JOIN RideInfo rideInfo ON ride.rideId = rideInfo.rideId AND rideInfo.isDriver "
-			+ "JOIN Location startLocation ON rideInfo.startLocationId = startLocation.locationId "
-			+ "JOIN Location endLocation ON rideInfo.endLocationId = endLocation.locationId "
 			+ "JOIN Vehicle vehicle on vehicle.vehicleId = ride.vehicleId "
+			+ "JOIN ride.geopoint geoPoint "
 			+ "WHERE ride.rideId = :rideId")
 
 	List<RideHeaderProjection> findRideHeaderInfoByRideId(@Param("rideId") Integer rideId);
@@ -86,11 +80,7 @@ public interface RideRepository extends JpaRepository<Ride, Integer> {
 			+ " JOIN Location startLocation ON startLocation.locationId = rideInfo.startLocationId"
 			+ " JOIN Location endLocation ON endLocation.locationId = rideInfo.endLocationId"
 			+ " JOIN Vehicle vehicle on vehicle.vehicleId = ride.vehicleId"
-			+ " LEFT JOIN (SELECT rideRequestId rideRequestId ,rideId rideId , riderId riderId, driverId driverId, requestStatus requestStatus FROM RideRequestInfo "
-			+ "            WHERE riderId = :userId AND rideId in :rideIds "
-			+ "            GROUP BY rideId,riderId,rideRequestId,requestStatus "
-			+ "            ORDER BY createdTime DESC LIMIT 1) AS rideRequest "
-			+ "			   ON rideRequest.driverId = rideInfo.userId AND rideRequest.rideId = ride.rideId AND rideRequest.riderId = :userId"
+			+ " LEFT JOIN RideRequestInfo rideRequest ON rideRequest.driverId = rideInfo.userId AND rideRequest.rideId = ride.rideId AND rideRequest.riderId = :userId"
 			+ " WHERE rideInfo.userId != :userId AND ride.status != 'completed' AND ride.startTime >= :rideTimeStartLimit AND ride.startTime <= :rideTimeEndLimit AND ride.rideId IN :rideIds"
 			+ " ORDER BY ride.startTime")
 	List<SearchResultProjection>findRideDetailsByRideIds(@Param("rideIds") List<Integer> rideIds, @Param("rideTimeStartLimit") LocalDateTime rideTimeStartLimit, @Param("rideTimeEndLimit") LocalDateTime rideTimeEndLimit, @Param("userId") Integer userId);
