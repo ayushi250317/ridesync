d7f8b08ecb0e06bfd24839092eac4c7af3f0a471
nithinbharathi <nithinbharathi@dal.ca>
2024-03-23 09:08:03 -0300

    Code refactor: removed the unused getRide api call and its references

--- ridesync_backend/src/main/java/com/app/ridesync/dto/responses/RideInfoResponse.java
+++ ridesync_backend/src/main/java/com/app/ridesync/dto/responses/RideInfoResponse.java
@@ -5,20 +5,48 @@ import com.app.ridesync.entities.RideInfo;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class RideInfoResponse {
-	
+
 	private Location location1;
 	private Location location2;
 	private RideInfo rideInfo;
-//	private String message;
-//    private boolean success;
 	private Location pickupLocation;
+
+	public Location getLocation1() {
+		return location1;
+	}
+
+	public void setLocation1(Location location1) {
+		this.location1 = location1;
+	}
+
+	public Location getLocation2() {
+		return location2;
+	}
+
+	public void setLocation2(Location location2) {
+		this.location2 = location2;
+	}
+
+	public RideInfo getRideInfo() {
+		return rideInfo;
+	}
+
+	public void setRideInfo(RideInfo rideInfo) {
+		this.rideInfo = rideInfo;
+	}
+
+	public Location getPickupLocation() {
+		return pickupLocation;
+	}
+
+	public void setPickupLocation(Location pickupLocation) {
+		this.pickupLocation = pickupLocation;
+	}
 
 }
